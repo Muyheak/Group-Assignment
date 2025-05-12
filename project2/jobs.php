203697f52baf4f6428da55434c9c4113c0e5ede1
Tomas <105320237@student.swin.edu.au>
2025-05-12 22:30:55 +1000

    Links fixed and apply page to php

--- project2/jobs.php
+++ project2/jobs.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Job Descriptions</title>
-  <link href="./styles/style.css" rel="stylesheet">
+  <link href="../styles/style.css" rel="stylesheet">
 </head>
 
 <body>
