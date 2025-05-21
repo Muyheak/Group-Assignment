a4e6b2e496a448b91d877c067cbed5aa094515f4
Cj Fu <105927887@student.swin.edu.au>
2025-05-21 23:20:03 +1000

    Added login.php and logout.php for manage.php

---
+++ project2/login.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    if (isset($_SESSION['authenticated']) && $_SESSION['authenticated']) {
+        header("Location: ./manage.php");
+    }
+
+    require_once("settings.php");
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+?>
+
+<?php include 'header.inc'; ?>
+
+<?php include 'nav.inc'; 
+        require_once("settings.php");
+        
+            $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+            if (!$conn) {
+                echo "<p>Database connection failure</p>";
+            }
+?>
+
+    <div class="loginpage">
+        <form action="authentication.php" method="post" class="loginform" novalidate>
+            <h2>Manager Login</h2>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+    
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password">
+
+            <input type="hidden" name="token" value="abc123">
+            <input type="submit" value="Login">
+
+<?php include 'includes/footer.inc'; ?>
+
+</body>
+
+</html>
