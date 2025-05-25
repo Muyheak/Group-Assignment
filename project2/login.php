5749a6a186848624ee31de5eaaf56f2bed56e747
Cj Fu <105927887@student.swin.edu.au>
2025-05-25 23:34:44 +1000

    Trying again..

--- project2/login.php
+++ project2/login.php
@@ -12,13 +12,15 @@
 <?php include 'nav.inc'; ?>
 
 <body>
-<div class="container">
-  <form method="post" action="Processing.php">
+<div class="login-wrapper">
+  <form method="post" action="Processing.php" class="login-form">
+      <h2>Log In</h2>
+
       <label for="username">Username:</label>
-      <input type="text" name="username" required><br>
+      <input type="text" name="username" id="username" required>
 
       <label for="password">Password:</label>
-      <input type="password" name="password" required><br>
+      <input type="password" name="password" id="password" required>
 
       <input type="hidden" name="token" value="abc123">
       <input type="submit" value="Login" class="login-button">
