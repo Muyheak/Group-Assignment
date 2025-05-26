ab0c0ac47a97aa06ec0696820a2bb8f35c06a70e
Muyheak <105231643@student.swin.edu.au>
2025-05-26 21:06:31 +1000

    cmt

--- project2/jobs.php
+++ project2/jobs.php
@@ -1,14 +1,14 @@
 <?php
-// Step 1: Connect to the database
+
 $servername = "localhost";
 $username = "root"; 
 $password = "";     
 $dbname = "jobs";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
